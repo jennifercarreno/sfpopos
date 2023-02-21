ccb809353be21f7ce987cc406b8e4e9b55a40399
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-21 14:01:13 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,22 @@
+// src/Title.js
+import './Title.css';
+
+function Title() {
+  
+    return (
+        <div>
+<div className="Title">
+            <h1>SFPOPOS</h1>
+
+        </div>
+        <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+        </div>
+        
+
+        
+        
+
+        )
+  }
+  
+  export default Title
