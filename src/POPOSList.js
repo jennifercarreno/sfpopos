23f3f970798b3d73d60bfddb43f8c0889838286e
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-21 14:19:09 -0800

    content component built

---
+++ src/POPOSList.js
@@ -0,0 +1,42 @@
+// src/POPOSList.js
+import POPOSSpace from './POPOSSpace';
+
+function POPOSList() {
+    return (
+      <div>
+        <POPOSSpace
+            name="50 California Street"
+            address="50 California St."
+            image="50-california-st.jpg"
+            />
+        <POPOSSpace
+            name="100 Pine"
+            address="100 Pine St."
+            image="100-pine.jpg"
+            />
+        <POPOSSpace
+            name="101 California"
+            address="101 California St."
+            image="101-california.jpg"
+            />
+        <POPOSSpace
+            name="343 Sansome Roof Garden"
+            address="100 Sansome St."
+            image="343-sansome-roof-garden.jpg"
+            />
+        <POPOSSpace
+            name="525 Market St Plaza"
+            address="525 Market St."
+            image="525-market-street-plaza.jpg"
+            />
+        <POPOSSpace
+            name="555 California"
+            address="555 California St."
+            image="555-california.jpg"
+            />
+
+      </div>
+    )
+  }
+  
+  export default POPOSList
