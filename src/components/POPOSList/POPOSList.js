05846baac7c9858e8f7b66cfd56684c862cb1b48
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-21 19:12:55 -0800

    implemented search

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,24 +1,37 @@
 // src/POPOSList.js
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
-import data from '../../sfpopos-data.json'
+import data from '../../sfpopos-data.js'
+import { useState } from 'react'
+
 function POPOSList() {
 
-    const spaces = data.map(({ title, address, images, hours }, i) => {
-        return (
-          <POPOSSpace
-            id={i}
-            key={title}
-            name={title}
-            address={address}
-            image={images[0]}
-            hours={hours}
-          />
-        )
-      })
+  const [ query, setQuery ] = useState('')
+  const spaces = data
+  .filter(obj => obj.title.toLowerCase().includes(query.toLowerCase()) || obj.address.toLowerCase().includes(query.toLowerCase()))
+  .map(({ id, title, address, images, hours }, i) => {
+      return (
+        <POPOSSpace
+          id={id}
+          key={title}
+          name={title}
+          address={address}
+          image={images[0]}
+          hours={hours}
+        />
+      )
+    })
   
     return (
       <div className="POPOSList">
+        <form>
+				<input
+					value={query}
+					placeholder="search"
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
         { spaces }
       </div>
     )
