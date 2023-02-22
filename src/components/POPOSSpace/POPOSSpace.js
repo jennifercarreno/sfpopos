65cf984823d26adc45f79f83dca456659f8926e6
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-21 18:46:18 -0800

    styled spaces

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -11,12 +11,14 @@ function POPOSSpace(props) {
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
         </Link>
         <h1>
-            <Link to={`/details/${id}`}>
+            <Link className="POPOSSpace-title" to={`/details/${id}`}>
                 {name}
             </Link>
         </h1>
-        <div>{address}</div>
-        <div className="hours">{hours}</div>
+        <div className="POPOSSpace-info">
+				<div >{address}</div>
+				<div>{hours}</div>
+		</div>
     </div>
     )
 }
